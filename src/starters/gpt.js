26c8cdb41093e342abc9074778c58f381f349e17
Nicat <59221034+Nicat-dcw@users.noreply.github.com>
2023-02-05 21:44:12 +0400

    Update gpt.js

--- src/starters/gpt.js
+++ src/starters/gpt.js
@@ -1,5 +1,7 @@
 module.exports = async (client) => {
 const { ChatGPTAPI } = await import('chatgpt')
+const fetch = require("node-fetch")
+global.fetch = fetch;
 const api = new ChatGPTAPI({
     apiKey: client.config.OPENAI_API_KEY
   })
